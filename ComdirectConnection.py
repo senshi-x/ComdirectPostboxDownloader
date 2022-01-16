5ee8ac137129ea0d4911c29e59c756ef734887b4
WGPSenshi <6715079+WGPSenshi@users.noreply.github.com>
2022-01-16 23:04:08 +0100

    improved user feedback on faulty userdata during login

--- ComdirectConnection.py
+++ ComdirectConnection.py
@@ -70,9 +70,18 @@ class Connection:
         )
 
         try:
-            rjson = r.json()
-            self.access_token = rjson["access_token"]
-            self.refresh_token = rjson["refresh_token"]
+            if r.status_code == 200:
+                rjson = r.json()
+                self.access_token = rjson["access_token"]
+                self.refresh_token = rjson["refresh_token"]
+            else:
+                status = r.status_code
+                reason = ""
+                if status == 401:
+                    reason = "This usually means wrong clientID/clientSecret"
+                elif status == 400:
+                    reason = "This usually means wrong username/pwd"
+                print(f"HTTP Status: {r.status_code} | {r.json()['error_description']} | {reason}")
         except Exception as err:
             raise err
 
@@ -90,8 +99,7 @@ class Connection:
             except Exception as err:
                 raise err
         else:
-            print(r.status_code)
-            print(r.json())
+            print(f"HTTP Status: {r.status_code} | {r.json()}")
 
     def __getTANChallenge(self):
         """
@@ -114,8 +122,7 @@ class Connection:
         if r.status_code == 201:
             self.__getSessionTAN(r.headers)
         else:
-            print(r.status_code)
-            print(r.json)
+            print(f"HTTP Status: {r.status_code} | {r.json()}")
 
     def __getSessionTAN(self, validationHeaders):
         """
@@ -164,8 +171,7 @@ class Connection:
             headers=headers,
         )
         if r.status_code != 200:
-            print(r.status_code)
-            print(r.json())
+            print(f"HTTP Status: {r.status_code} | {r.json()}")
 
     def __challenge_ptan(self, challenge):
         """
